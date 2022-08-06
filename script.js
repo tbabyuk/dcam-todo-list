472c83906e96610d9e7fc6e293554ea67f96a54c
Terry Babyuk <tbabyuk@gmail.com>
2022-08-06 18:32:39 -0400

    added priority options to list items, character limit to additem field, error message

--- script.js
+++ script.js
@@ -1,54 +1,71 @@
+const searchToDos = document.querySelector('.search-todos');
+const toDoList = document.querySelector('.todo-list');
+const addToDos = document.querySelector('.add-todo');
+const items = document.querySelectorAll('toDoList[li]');
+const error = document.querySelector('.error');
 
-const addToDoForm = document.querySelector(".add-todo");
-const toDoList = document.querySelector(".todo-list");
-const searchToDos = document.querySelector(".search-todos");
-const items = document.querySelectorAll("toDoList[li]")
 
+// FUNCTIONS
 
-// filtering todo items based on search
-searchToDos.addEventListener('input', (e) => {
-    let query = e.target.value.toLowerCase();
-
-    let items = Array.from(toDoList.children);
-
-    items.forEach(item => {
-        let itemText = item.textContent.trim().toLowerCase();
-
-        if(!itemText.includes(query)) {
-            item.classList.add('hidden')
-        } else {
-            item.classList.remove('hidden')
-        }
-    })
-})
-
-// creating template for todo item
+// create a todo item
 const createToDoItem = (text) => {
-    const html = `
-    <li class="list-group-item mb-1 rounded d-flex justify-content-between align-items-center">
-    <span>${text}</span>
-    <i class="far fa-trash-alt delete"></i>
-    </li>    
-`;
-    toDoList.innerHTML += html;
-}
+  const html = `
+      <li class="list-group-item mb-1 rounded d-flex justify-content-between align-items-center">
+      <span>${text}</span>
+      <select class="choose-priority rounded">
+          <option class="low" value="low">LOW</option>
+          <option class="medium" value="medium">MEDIUM</option>
+          <option class="high" value="high">HIGH</option>
+      </select>
+      <i class="far fa-trash-alt delete"></i>
+      </li>    
+  `;
+  toDoList.innerHTML += html;
+};
 
 
-// creating a todo item
-addToDoForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = addToDoForm.search.value;
-    if(text) {
-        createToDoItem(text);
+// EVENT LISTENERS
+
+// filter todo items based on search query
+searchToDos.addEventListener('input', (e) => {
+  let query = e.target.value.toLowerCase();
+
+  let items = Array.from(toDoList.children);
+
+  items.forEach((item) => {
+    let itemText = item.textContent.trim().toLowerCase();
+
+    if (!itemText.includes(query)) {
+      item.classList.add('hidden');
+    } else {
+      item.classList.remove('hidden');
     }
-    addToDoForm.reset();
+  });
 });
 
+// add a todo item
+addToDos.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const text = addToDos.search.value;
+  if (text) {
+    createToDoItem(text);
+  }
+  addToDos.reset();
+});
 
-// deleting a todo item
+// listen for length of add item input field
+addToDos.addEventListener('input', (e) => {
+  const inputLength = e.target.value.length;
+  console.log(inputLength);
+  inputLength > 39
+    ? error.classList.remove('hidden')
+    : error.classList.add('hidden');
+});
+
+// delete a todo item
 toDoList.addEventListener('click', (e) => {
-    if(e.target.classList.contains("delete")) {
-        const parent = e.target.parentElement;
-        parent.remove();
-    }
-})
+  if (e.target.classList.contains('delete')) {
+    const parent = e.target.parentElement;
+    parent.remove();
+  }
+});
