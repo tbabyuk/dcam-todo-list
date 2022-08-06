930ddda7f6f5aae52c3ad93191b6fcf0e4033db0
Terry Babyuk <tbabyuk@gmail.com>
2022-08-06 13:49:22 -0400

    list filter functionality working

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 
 const addToDoForm = document.querySelector(".add-todo");
 const toDoList = document.querySelector(".todo-list");
+const searchToDos = document.querySelector(".search-todos");
+const items = document.querySelectorAll("toDoList[li]")
 
 
+// filtering todo items based on search
+searchToDos.addEventListener('input', (e) => {
+    let query = e.target.value.toLowerCase();
+
+    let items = Array.from(toDoList.children);
+
+    items.forEach(item => {
+        let itemText = item.textContent.trim().toLowerCase();
+
+        if(!itemText.includes(query)) {
+            item.classList.add('hidden')
+        } else {
+            item.classList.remove('hidden')
+        }
+    })
+})
+
+// creating template for todo item
 const createToDoItem = (text) => {
     const html = `
     <li class="list-group-item mb-1 rounded d-flex justify-content-between align-items-center">
@@ -14,6 +34,7 @@ const createToDoItem = (text) => {
 }
 
 
+// creating a todo item
 addToDoForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const text = addToDoForm.search.value;
@@ -24,6 +45,7 @@ addToDoForm.addEventListener('submit', (e) => {
 });
 
 
+// deleting a todo item
 toDoList.addEventListener('click', (e) => {
     if(e.target.classList.contains("delete")) {
         const parent = e.target.parentElement;
