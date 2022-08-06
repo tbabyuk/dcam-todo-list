d25eae7ff33daad47fd095f688135132741b4a76
Terry Babyuk <tbabyuk@gmail.com>
2022-08-06 02:08:57 -0400

    more UI styling, item delete functionality working

--- script.js
+++ script.js
@@ -5,16 +5,28 @@ const toDoList = document.querySelector(".todo-list");
 
 const createToDoItem = (text) => {
     const html = `
-    <li class="list-group-item">${text}</li>
-    `;
+    <li class="list-group-item mb-1 rounded d-flex justify-content-between align-items-center">
+    <span>${text}</span>
+    <i class="far fa-trash-alt delete"></i>
+    </li>    
+`;
     toDoList.innerHTML += html;
 }
-
 
 
 addToDoForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const text = addToDoForm.search.value;
-    createToDoItem(text);
+    if(text) {
+        createToDoItem(text);
+    }
     addToDoForm.reset();
+});
+
+
+toDoList.addEventListener('click', (e) => {
+    if(e.target.classList.contains("delete")) {
+        const parent = e.target.parentElement;
+        parent.remove();
+    }
 })
